0bc6a2a464cc8a4dbed9125809069a01270cc3e8
Yusuf <101695654+YusufAlakus@users.noreply.github.com>
2024-07-04 22:24:11 +0300

    Add files via upload

---
+++ ChatbotYaver/SignupActivity.java
@@ -0,0 +1,82 @@
+package com.example.yaver;
+
+import android.content.Intent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignupActivity extends AppCompatActivity {
+
+    EditText signupName, signupEmail, signupUsername, signupPassword;
+    TextView loginRedirectText;
+    Button signupButton;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signup);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        FirebaseApp.initializeApp(this);
+        signupName = findViewById(R.id.signup_name);
+        signupEmail =findViewById(R.id.signup_email);
+        signupUsername =findViewById(R.id.signup_username);
+        signupPassword =findViewById(R.id.signup_password);
+        signupButton =findViewById(R.id.signup_button);
+        loginRedirectText=findViewById(R.id.loginRedirectText);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("users");
+
+                String name = signupName.getText().toString();
+                String email =signupEmail.getText().toString();
+                String username =signupUsername.getText().toString();
+                String password =signupPassword.getText().toString();
+
+                HelperClass helperClas = new HelperClass(name,email,username,password);
+                reference.child(name).setValue(helperClas);
+
+                Toast.makeText(SignupActivity.this,"You have signed up successfully!",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
